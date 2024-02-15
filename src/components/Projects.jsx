3a72bb60733ed2112450ce190b9e54f863fd21dd
Douglas Moreira <dougmoreira@Douglass-MBP.lan>
2024-02-15 12:25:32 -0500

    node version update

---
+++ src/components/Projects.jsx
@@ -0,0 +1,81 @@
+import React from 'react';
+
+const Projects = () =>{
+    return(
+        <>
+        <div className="projects" id="PROJECTS">
+            <div className="projects-head">
+                <p className="code-text">{"<"}h2{">"}</p>
+                <h2>Projects</h2>
+                <p className="code-text">{"<"}/h2{">"}</p>
+            </div>
+            <p className="code-text">{"<"}div id="applications-I-build"{">"}</p>
+            <div className="project-section">
+                <div className="project-box">
+                    <h3>Pokedex</h3>
+                    <hr className="mini-bar"/>
+                    <img src="https://i.ibb.co/LCLgMy3/Pokemon.gif"/>
+                    <p>In this first tech Project, I combine my HTML,
+                        CSS and JavaScript skills to create a Pokedex
+                        where the user can search for pokemons and get
+                        all their information. I utilized user stories
+                            and wireframes to scope the project and 
+                            incorporate MVP. For programming languages, 
+                            I used JavaScript and jQuery to manipulate the
+                            DOM and CSS to give an immersive experience to
+                            the user.</p>
+                            <button className="project-btn"><a href="https://pokemon-world-imdoug.netlify.app/" target="_blank">SEE NOW</a></button>
+                </div>
+                <div className="project-box">
+                    <h3>Amores Restaurant</h3>
+                    <hr className="mini-bar"/>
+                    <img src="https://i.ibb.co/316R3xB/Amores-Restaurant.gif"/>
+                    <p>This was a project that combined Mongoose, 
+                        Express and Node.js (MEN) tech stack to create
+                        a Server-side Application that leveraged 
+                        two-model, MongoDB database. This project was
+                        focused on CRUD functionality, RESTful routing
+                        and MVC methodologies. I also utilized plain CSS
+                            to style the application and used EJS to language
+                            to build the functionality and logic of the project.</p>
+                            <button className="project-btn"><a href="https://amores-restaurant.herokuapp.com/restaurant" target="_blank">SEE NOW</a></button>
+                </div>
+                {/* <div className="project-box">
+                    <h3>DevMuscle</h3>
+                    <hr className="mini-bar"/>
+                    <img src="https://i.ibb.co/K9C6JFB/DEVMuscles.gif"/>
+                    <p>This was  a pair programming project that
+                        Incremented React in the MEN stack forming
+                        a MERN tech stack, this application leveraged
+                        one model, Mongo DB database. Focusing to use
+                            React as frontEnd framework and Express and 
+                            Node.js for the backEnd work. This project was
+                            also an exercise in Git collaboration, building
+                            developer branches, submitting pull requests
+                            and merging code.
+                            </p>
+                            <button className="project-btn"><a href="https://devmuscle.herokuapp.com/" target="_blank">SEE NOW</a></button>
+                </div> */}
+                <div className="project-box">
+                    <h3>Lost Paws</h3>
+                    <hr className="mini-bar"/>
+                    <img src="https://i.ibb.co/hB9FkLX/LostPaws.gif"/>
+                    <p>This project I challenged myself to learn something new,
+                        and  I learned Java and Spring Boot to create the 
+                        API/backEnd for this application. On the frontEnd I
+                        used React with useContext and Router to give an 
+                        app visual to this application. It is a full CRUD
+                        application with RESTful API. The project itself is
+                        a web application for lost dogs to find their owners/family
+                        .</p>
+                            <button className="project-btn"><a href="https://lost-paws-front.herokuapp.com/" target="_blank">SEE NOW</a></button>
+                </div>
+            </div>
+            <p className="code-text">{"<"}/div{">"}</p>
+            <p className="code-text last">{"<"}!-- like what you see? hire me! --{">"}</p>
+        </div>
+        </>
+    )
+}
+
+export default Projects
---
+++ src/components/Skills.jsx
@@ -0,0 +1,136 @@
+import React from "react";
+
+const Skills = () => {
+  return (
+    <>
+      <div className="skills-container" id="SKILLS">
+        <div className="skills-head">
+          <p className="code-text">
+            {"<"}/h2{">"}
+          </p>
+          <h2 className="section-title">My Skills</h2>
+          <p className="code-text">
+            {"<"}/h2{">"}
+          </p>
+        </div>
+        <p className="code-text">
+          {"<"}div id="all-i've-learned"{">"}
+        </p>
+        <div className="skills">
+          <div className="skill-box">
+            <div className="top">
+              <i class="fa fa-circle"></i>
+              <i class="fa fa-circle"></i>
+              <i class="fa fa-circle"></i>
+            </div>
+            <hr />
+            <div className="skill-text">
+              <h3 className="skill-title">Languages</h3>
+              <p className="skill-item">JavaScript</p>
+              <p className="skill-item">Java</p>
+              <p className="skill-item">Ruby</p>
+              <p className="skill-item">Python</p>
+            </div>
+          </div>
+          <div className="skill-box">
+            <div className="top">
+              <i class="fa fa-circle"></i>
+              <i class="fa fa-circle"></i>
+              <i class="fa fa-circle"></i>
+            </div>
+            <hr />
+            <div className="skill-text">
+              <h3 className="skill-title">Enviroments</h3>
+              <p className="skill-item">VS Code</p>
+              <p className="skill-item">Atom</p>
+              <p className="skill-item">PyCharm</p>
+            </div>
+          </div>
+          <div className="skill-box">
+            <div className="top">
+              <i class="fa fa-circle"></i>
+              <i class="fa fa-circle"></i>
+              <i class="fa fa-circle"></i>
+            </div>
+            <hr />
+            <div className="skill-text">
+              <h3 className="skill-title">Databases</h3>
+              <p className="skill-item">ProstgreSQL</p>
+              <p className="skill-item">SQL</p>
+              <p className="skill-item">MySQL</p>
+              <p className="skill-item">Mongo DB</p>
+            </div>
+          </div>
+          <div className="skill-box">
+            <div className="top">
+              <i class="fa fa-circle"></i>
+              <i class="fa fa-circle"></i>
+              <i class="fa fa-circle"></i>
+            </div>
+            <hr />
+            <div className="skill-text">
+              <h3 className="skill-title">Server-Side</h3>
+              <p className="skill-item">Express</p>
+              <p className="skill-item">Django</p>
+              <p className="skill-item">Node.js</p>
+              <p className="skill-item">Spring Boot</p>
+            </div>
+          </div>
+          <div className="skill-box">
+            <div className="top">
+              <i class="fa fa-circle"></i>
+              <i class="fa fa-circle"></i>
+              <i class="fa fa-circle"></i>
+            </div>
+            <hr />
+            <div className="skill-text">
+              <h3 className="skill-title">Front End</h3>
+              <p className="skill-item">ReactJs</p>
+              <p className="skill-item">Svelte</p>
+              <p className="skill-item">HTML</p>
+              <p className="skill-item">CSS</p>
+              <p className="skill-item">Sass</p>
+            </div>
+          </div>
+          <div className="skill-box">
+            <div className="top">
+              <i class="fa fa-circle"></i>
+              <i class="fa fa-circle"></i>
+              <i class="fa fa-circle"></i>
+            </div>
+            <hr />
+            <div className="skill-text">
+              <h3 className="skill-title">Version Control</h3>
+              <p className="skill-item">Github</p>
+              <p className="skill-item">Git</p>
+              <p className="skill-item">Bitbucket</p>
+            </div>
+          </div>
+          <div className="skill-box">
+            <div className="top">
+              <i class="fa fa-circle"></i>
+              <i class="fa fa-circle"></i>
+              <i class="fa fa-circle"></i>
+            </div>
+            <hr />
+            <div className="skill-text">
+              <h3 className="skill-title">Methodologies</h3>
+              <p className="skill-item">Object oriented program</p>
+              <p className="skill-item">MVC</p>
+              <p className="skill-item">F.I.R.S.T principles</p>
+              <p className="skill-item">C.R.A.P Design </p>
+            </div>
+          </div>
+        </div>
+        <p className="code-text">
+          {"<"}/div{">"}
+        </p>
+        <p className="code-text last">
+          {"<"}!-- I'm a fast learner --{">"}
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default Skills;
